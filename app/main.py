296fec0d7b1b0449909f6fc1e5a289241c688019
Chathura Madhusanka <madhusanka.slc@gmail.com>
2026-03-14 15:54:15 +0530

    FastAPI Git & pre-commit

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import pathlib
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+BASE_DIR = pathlib.Path(__file__).parent
+print((BASE_DIR / "templates").exists())
 
 app = FastAPI()
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
-@app.get("/")
-def home_view():
-    return {"message": "FastAPI is running.."}
+@app.get("/", response_class=HTMLResponse)
+def home_view(request: Request):
+    print(request)
+    return templates.TemplateResponse("home.html", {"request": request, "abc":10 })
 
 
 @app.post("/")
