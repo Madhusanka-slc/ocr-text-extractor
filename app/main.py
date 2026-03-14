d0f93a2c6f6b6ae9425f1d9b8f02f29809c595f5
Chathura Madhusanka <madhusanka.slc@gmail.com>
2026-03-14 17:50:44 +0530

    Implement ocr predictions

--- app/main.py
+++ app/main.py
@@ -16,14 +16,19 @@ from fastapi.templating import Jinja2Templates
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from functools import lru_cache
 from PIL import Image
+import pytesseract
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
-        extra="ignore"   # 👈 prevents crash
+        extra="ignore"   # 
     )
 
+    app_auth_token: str
     debug: bool = False
+    echo_active: bool = False
+    app_auth_token_prod: str = None
+    skip_auth: bool = False
 
 
 @lru_cache
@@ -43,13 +48,34 @@ templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
 @app.get("/", response_class=HTMLResponse) # http GET -> JSON
 def home_view(request: Request, settings:Settings = Depends(get_settings)):
-    print(settings)
     return templates.TemplateResponse("home.html", {"request": request, "abc": 123})
 
 
-@app.post("/")
-def home_detail_view():
-    return {"message": "FastAPI is running.."}
+def verify_auth(authorization = Header(None), settings:Settings = Depends(get_settings)):
+    """
+    Authorization: Bearer <token>
+    {"authorization": "Bearer <token>"}
+    """
+    if settings.debug and settings.skip_auth:
+        return
+    if authorization is None:
+        raise HTTPException(detail="Invalid endpoint", status_code=401)
+    label, token = authorization.split()
+    if token != settings.app_auth_token:
+        raise HTTPException(detail="Invalid endpoint", status_code=401)
+
+
+@app.post("/") # http POST
+async def prediction_view(file:UploadFile = File(...), authorization = Header(None), settings:Settings = Depends(get_settings)):
+    verify_auth(authorization, settings)
+    bytes_str = io.BytesIO(await file.read())
+    try:
+        img = Image.open(bytes_str)
+    except:
+        raise HTTPException(detail="Invalid image", status_code=400)
+    preds = pytesseract.image_to_string(img)
+    predictions = [x for x in preds.split("\n")]
+    return {"results": predictions, "original": preds}
 
 
 @app.post("/img-echo/", response_class=FileResponse) # http POST
@@ -58,14 +84,12 @@ async def img_echo_view(file:UploadFile = File(...), settings:Settings = Depends
         raise HTTPException(detail="Invalid endpoint", status_code=400)
     UPLOAD_DIR.mkdir(exist_ok=True)
     bytes_str = io.BytesIO(await file.read())
-    # try:
-    #     img = Image.open(bytes_str)
-    # except:
-    #     raise HTTPException(detail="Invalid image", status_code=400)
+    try:
+        img = Image.open(bytes_str)
+    except:
+        raise HTTPException(detail="Invalid image", status_code=400)
     fname = pathlib.Path(file.filename)
     fext = fname.suffix # .jpg, .txt
     dest = UPLOAD_DIR / f"{uuid.uuid1()}{fext}"
-    # img.save(dest)
-    with open(str(dest), 'wb') as out:
-        out.write(bytes_str.read())
+    img.save(dest)
     return dest
